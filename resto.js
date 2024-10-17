fc8462642132d963089d16672df53d2b8d8b4597
Phanindhraaa <suraphanindhra@gmail.com>
2024-10-17 15:37:44 +0530

    Create resto.js

---
+++ resto.js
@@ -0,0 +1,5 @@
+// Basic form submission functionality
+document.getElementById('reservation-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your reservation! We will contact you soon.');
+});
